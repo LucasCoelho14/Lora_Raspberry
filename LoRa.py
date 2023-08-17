710bffd2f0fb1e6a52e5e64b88d77779055dd988
LucasCoelho14 <lucas.coelho@engenharia.ufjf.br>
2023-08-16 21:33:55 -0300

    LoraRaspberry

---
+++ LoRa.py
@@ -0,0 +1,42 @@
+import spidev
+import time
+
+# SPI configuration
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 500000
+
+# LoRa parameters
+FREQUENCY = 915e6  # Set your frequency here
+SPREADING_FACTOR = 7  # Spreading factor (7 to 12)
+TX_POWER = 14  # Power level from 2 to 20
+
+# Message to send
+message = "Hello, LoRa!"
+
+# LoRa register addresses
+REG_FIFO = 0x00
+REG_OP_MODE = 0x01
+REG_FR_MSB = 0x06
+REG_FR_MID = 0x07
+REG_FR_LSB = 0x08
+REG_PA_CONFIG = 0x09
+REG_FIFO_TX_BASE_ADDR = 0x0E
+REG_FIFO_ADDR_PTR = 0x0D
+REG_PAYLOAD_LENGTH = 0x22
+REG_IRQ_FLAGS = 0x12
+REG_MODEM_CONFIG1 = 0x1D
+REG_MODEM_CONFIG2 = 0x1E
+
+# Initialize LoRa module
+def lora_reset():
+    pass  # Implement your reset logic here
+
+def set_mode(mode):
+    spi.xfer2([REG_OP_MODE | 0x80, mode])
+
+def write_register(address, value):
+    spi.xfer2([address | 0x80, value])
+
+# Reset LoRa module
+lora_reset
