6fb45cfcb4e873c08b54b1e07386c5edbe116358
Lucas Coelho <93057396+LucasCoelho14@users.noreply.github.com>
2023-08-19 01:45:15 -0300

    Update LoRa.py

--- LoRa.py
+++ LoRa.py
@@ -1,29 +1,30 @@
-from sx127x import SX127x
-from sx127x.lib import config
-from sx127x.lib.rpi import GPIO
+from pyLoRa import LoRa
 import time
 
-class LoRaSender(SX127x):
-    def __init__(self, parameters, verbose=False):
-        super(LoRaSender, self).__init__(parameters, verbose)
+# Configuração dos pinos GPIO para LoRa
+cs_pin = 8      # GPIO8 - NSS
+reset_pin = 22  # GPIO22 - NRESET
+dio0_pin = 4    # GPIO4 - DIO0
+
+# Inicialização do módulo LoRa
+lora = LoRa(cs_pin, reset_pin, dio0_pin)
+
+try:
+    if lora.begin(freq=915E6):
+        print("Módulo LoRa está conectado!")
         
-    def send(self, data):
-        self.set_mode('tx')
-        self.write_payload(data)
-        self.set_dio_mapping([0, 0, 0, 0])
-        self.clear_irq_flags()
-        self.send_pkt_len(len(data))
-        self.set_payload_length(len(data))
-        self.set_mode('tx')
-        while self.get_irq_flags()['tx_done'] == 0:
-            time.sleep(0.5)
+        # Mensagem a ser enviada
+        message = "Oi ESP32!"
 
-GPIO.setmode(GPIO.BCM)
-lora = LoRaSender(channel=0, RST=22, verbose=False)
-lora.set_mode('sleep')
+        # Envia a mensagem
+        lora.send_data(message)
+        print(f"Mensagem enviada: {message}")
 
-message = "Hello, ESP32!"
-lora.send(message.encode())
+    else:
+        print("Não foi possível inicializar o módulo LoRa.")
+        
+except KeyboardInterrupt:
+    pass
 
-lora.set_mode('sleep')
-GPIO.cleanup()
+finally:
+    lora.close()
