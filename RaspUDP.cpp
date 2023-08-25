c996f37f171d4d2d463e438542f1a97882630711
Lucas Coelho <93057396+LucasCoelho14@users.noreply.github.com>
2023-08-25 13:31:33 -0300

    Update RaspUDP.cpp

--- RaspUDP.cpp
+++ RaspUDP.cpp
@@ -1,30 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
+#include <iostream>
+#include <cstring>
 #include <wiringPi.h>
-#include <wiringPiI2C.h>
-#include <wiringPiSPI.h>
-#include <wiringPiSerial.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-#define UDP_PORT 1234
+const int udpPort = 1234;
+
+int udpSocket;
 
 int main() {
-    if (wiringPiSetup() == -1) {
-        fprintf(stderr, "Failed to initialize WiringPi\n");
-        return 1;
-    }
+    std::cout << "Initializing WiringPi..." << std::endl;
+    wiringPiSetup();
 
-    int udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    std::cout << "Creating UDP socket..." << std::endl;
+    udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
     if (udpSocket == -1) {
         perror("socket");
         return 1;
     }
 
-    struct sockaddr_in serverAddr, clientAddr;
+    struct sockaddr_in serverAddr;
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(UDP_PORT);
+    serverAddr.sin_port = htons(udpPort);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(udpSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
@@ -32,21 +30,23 @@ int main() {
         return 1;
     }
 
-    printf("UDP listener started on port %d\n", UDP_PORT);
+    std::cout << "UDP listener started on port " << udpPort << std::endl;
 
     while (1) {
         char buffer[256];
+        struct sockaddr_in clientAddr;
         socklen_t clientAddrLen = sizeof(clientAddr);
+
         int bytesRead = recvfrom(udpSocket, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
         if (bytesRead == -1) {
             perror("recvfrom");
-            continue;
+        } else {
+            buffer[bytesRead] = '\0';
+            std::cout << "Received from " << inet_ntoa(clientAddr.sin_addr) << ": " << buffer << std::endl;
         }
 
-        buffer[bytesRead] = '\0';
-        printf("Received from %s: %s\n", inet_ntoa(clientAddr.sin_addr), buffer);
+        // Add your logic and delays here as needed.
     }
 
-    close(udpSocket);
     return 0;
 }
