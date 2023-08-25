067793b7d3502f199a5b4818ba52dc7ba4fb2311
Lucas Coelho <93057396+LucasCoelho14@users.noreply.github.com>
2023-08-25 19:10:02 -0300

    Update loraSend.cpp

--- loraSend.cpp
+++ loraSend.cpp
@@ -10,23 +10,6 @@
 #include <sys/ioctl.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
-#include <stdio.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-
-// #include <string>
-// #include <stdio.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <arpa/inet.h>
-// #include <string.h>
-// #include <sys/time.h>
-// #include <signal.h>
-// #include <stdlib.h>
-// #include <sys/ioctl.h>
-// #include <wiringPi.h>
-// #include <wiringPiSPI.h>
 
 
 // #############################################
